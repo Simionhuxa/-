e5eb16b409a3d133da1d925073efc27bdb1b3f6e
Simionhuxa <kn23-d.simionenko@nubip.edu.ua>
2024-07-08 11:50:30 +0300

    Add files via upload

---
+++ main№6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
+
+    printf("Введіть координати вектора послідовно через пробіл(x1 y1 x2 y2): ");
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    float length = sqrt((pow(dx, 2) + pow(dy, 2)));
+    printf("\nДовжина введеного вектора становить %.6f", length);
+    return 0;
+}
